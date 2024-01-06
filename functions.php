84c2dd1f346b7ffb98223bc9a673760b42bdfedd
Attila Horváth <atiradeon86@gmail.com>
2024-01-06 19:42:29 +0100

    .

--- functions.php
+++ functions.php
@@ -9,21 +9,6 @@ $sql = "SELECT id,event_name,color,link,start_date,end_date,StartTime,EndTime FR
 #$result = $conn->query($sql);  
 $result = sqlsrv_query( $conn, $sql);
 
-
-/*
-while ( $row = $result->fetch( PDO::FETCH_ASSOC ) ){
-    $eventArray[]= $row;
- }  
- echo json_encode($eventArray);
-
-}
-elseif (!empty($_GET['cmd']) && $_GET['cmd'] == 'addwork') {
-
-    print_r($_POST);
-    $json['status'] = true; 
-    echo json_encode($json);
-}
-*/
 while( $row = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC )) {
     $eventArray[]= $row;
 }
