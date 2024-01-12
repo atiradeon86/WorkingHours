076d1178fcc85d0accedbf1eccb3dc654c8f0a7e
Attila Horváth <atiradeon86@gmail.com>
2024-01-12 21:40:24 +0100

    apk with base function

---
+++ Android/BryanWorkingHours2/app/src/main/java/com/bryan/bryanworkinghours/BryanWv.kt
@@ -0,0 +1,36 @@
+package com.bryan.bryanworkinghours
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.bryan.bryanworkinghours.databinding.ActivityBryanWvBinding
+
+class BryanWv : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityBryanWvBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityBryanWvBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_bryan_wv)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_bryan_wv)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
---
+++ Android/BryanWorkingHours2/app/src/main/java/com/bryan/bryanworkinghours/FirstFragment.kt
@@ -0,0 +1,48 @@
+package com.bryan.bryanworkinghours
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import com.bryan.bryanworkinghours.databinding.FragmentFirstBinding
+
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val myWebView: WebView = view.findViewById(R.id.webview)
+
+
+        myWebView.loadUrl("https://apps.bryan86.hu")
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(WebViewClient())
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
