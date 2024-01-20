807f51d49a2e9b570113381f11df4876dd7bb4a3
Attila Horváth <atiradeon86@gmail.com>
2024-01-20 14:16:07 +0100

    E-mail Sending with pdf

--- admin/handler.php
+++ admin/handler.php
@@ -160,10 +160,10 @@ span.reason {
         //Server settings
         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'email-smtp.us-east-2.amazonaws.com';                     //Set the SMTP server to send through
+        $mail->Host       = '';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'AKIAUBBUHY3VTRGANKHI';                     //SMTP username
-        $mail->Password   = 'BGBX5EEVhSjvsRADNoxm/L2A8vDuwxDtSfYmhJTtscTg';                               //SMTP password
+        $mail->Username   = '';                     //SMTP username
+        $mail->Password   = '';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
